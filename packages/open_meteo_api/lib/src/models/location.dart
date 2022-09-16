304270f7eb95dd2e7e9a91abffc878ef89eb8c76
Mariugo Megiani <mariugomegiani@gmail.com>
2022-09-16 17:51:33 -0300

    Package requests done

--- packages/open_meteo_api/lib/src/models/location.dart
+++ packages/open_meteo_api/lib/src/models/location.dart
@@ -11,8 +11,25 @@
   ]
 }
 
+In order for code generation to work, we need to annotate our code using the following:
+
+@JsonSerializable to label classes which can be serialized
+@JsonKey to provide string representations of field names
+@JsonValue to provide string representations of field values
+Implement JSONConverter to convert object representations into JSON representations
+For each file we also need to:
+
+Import json_annotation
+Include the generated code using the part keyword
+Include fromJson methods for deserialization
+
 */
 
+import 'package:json_annotation/json_annotation.dart';
+
+part 'location.g.dart';
+
+@JsonSerializable()
 class Location {
   final int id;
   final String name;
@@ -25,4 +42,7 @@ class Location {
     required this.latitude,
     required this.longitude,
   });
+
+  factory Location.fromJson(Map<String, dynamic> json) =>
+      _$LocationFromJson(json);
 }

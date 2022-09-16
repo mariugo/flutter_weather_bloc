54007c80879f299a9a6bd003868ebd053adac4bd
Mariugo Megiani <mariugomegiani@gmail.com>
2022-09-16 19:26:24 -0300

    Repo imp done

--- packages/weather_repository/lib/src/models/weather.dart
+++ packages/weather_repository/lib/src/models/weather.dart
@@ -13,19 +13,21 @@ enum WeatherCondition {
 
 @JsonSerializable()
 class Weather extends Equatable {
-  final String location;
-  final double temperature;
-  final WeatherCondition condition;
-  Weather({
+  const Weather({
     required this.location,
     required this.temperature,
     required this.condition,
   });
 
+  factory Weather.fromJson(Map<String, dynamic> json) =>
+      _$WeatherFromJson(json);
+
+  Map<String, dynamic> toJson() => _$WeatherToJson(this);
+
+  final String location;
+  final double temperature;
+  final WeatherCondition condition;
+
   @override
-  List<Object?> get props => [
-        location,
-        temperature,
-        condition,
-      ];
+  List<Object> get props => [location, temperature, condition];
 }
